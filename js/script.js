9306c60f921d18f7e198e6f89ff84c72280c2493
mayaj0yce <m.j.thayne@gmail.com>
2023-06-12 17:34:08 -0600

    adding notes outside of argv

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log('hello')
+console.log(process.argv);
+
+
+function varAssign() {
+var Apple = process.argv [2];
+var Pears = process.argv [3];
+console.log(Apple)
+}
+varAssign();
+// this comments the function outside of the process.argv console log
+
+
+// if (Apple === Pears){
+// true () 
+    
+// } else {
+//     false
+// }

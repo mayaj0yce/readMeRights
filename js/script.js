7b541cfb8b7d2006282ec5348f7161f381703674
mayaj0yce <m.j.thayne@gmail.com>
2023-06-19 18:31:50 -0600

    work the script is generating questions html needs to be tidied but it has structure need to get the generateMarkdown.js folder and the script.js folder to meld together.

--- js/script.js
+++ js/script.js
@@ -1,19 +1,11 @@
-// import fs 
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-// add to the end of a file
-fs.appendFile('readMe.md', `${process.argv[2]}\n`, (err) =>
-    err ? console.error(err) : console.log('added')
-);
+//gen html
+const generateHTML = ({ title, description, installation, Usage, test, contributing, license, email, GitHubLink }) =>
+    ` html here `;
 
-
-// const license = [ array of licenses that can be pulled from]
-//this is where the personal information is inserted
-const generateHTML = ({ title, description, installation, Usage,  test, contributing, license, email, GitHubLink}) =>
-  ` html here `;
-
-
+//questions
 inquirer
     .prompt([
         {
@@ -36,15 +28,13 @@ inquirer
             name: "Usage",
             message: "How can this be used",
         },
-        
-        
         {
-            type: "input",            
+            type: "input",
             name: "Test",
             message: "What Tests have been done?",
         },
         {
-            type: "input",            
+            type: "input",
             name: "Contributing",
             message: "",
         },
@@ -52,15 +42,15 @@ inquirer
             type: "list",
             name: "license",
             message: "choose your license",
-            choices: ['none','Apache License 2.0','MIT License','The Unlicensed'],
+            choices: ['none', 'Apache License 2.0', 'MIT License'],
         },
         {
-            type: "input",           
+            type: "input",
             name: "Email",
             message: "Enter a Valide Email for Contact information",
         },
         {
-            type: "input",           
+            type: "input",
             name: "GitHubLink",
             message: "Provide GitHub LINK",
         },
@@ -73,40 +63,3 @@ inquirer
             err ? console.log(err) : console.log('Successfully created index.html!')
         );
     });
-
-    const renderLicense = (license) => {
-        return [![license: ${license}]
-  
-      },
-
-///fix that ^^^^^
-
-
-//add in the html code here if i want to use just JS
-//need to use the `${input}` from prompts inserted into the html^
-//html that is just a readme? can i use a readME.md or do I need to use the html format
-
-
-//my code ^^^
-
-// TODO: What are we importing?
-// const fs = require('fs');
-
-// TODO: Add comments to explain each of the three arguments of appendFile()
-// fs.appendFile('log.txt', `${process.argv[2]}\n`, (err) =>
-//   // TODO: Describe how this ternary operator works
-//   err ? console.error(err) : console.log('Commit logged!')
-// );
-
-console.log(`${process.argv[2]}\n`)
-
-// console.log(process.argv);
-
-
-// function varAssign() {
-// var Apple = process.argv [2];
-// var Pears = process.argv [3];
-// console.log(Apple)
-// }
-// varAssign();
-// this comments the function outside of the process.argv console log
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,45 +1,44 @@
 const inquirer = require('inquirer');
 
-function handleChoice(choice) {
+function chooseLicense(license) {
   // Mapping of choices to additional information
-  const additionalInfo = {
-    "A": "Additional information for 'A'.",
-    "B": "Additional information for 'B'.",
-    "C": "Additional information for 'C'.",
-    // Add more choices and additional information here
+  const addChoiceEnd = {
+    "None": "No License was chosen. Copyright Restrictions: Without a license, users have no permissions to use, modify, or distribute the software, and they must seek explicit permission from the copyright holder.",
+    "Apache License": "apache-2.0. Permissive: The Apache License is also a permissive license that allows users to use, modify, and distribute the software. It includes a patent grant, providing protection against patent claims for users of the software. ",
+    "MIT": "mit. It requires that the original copyright notice and license terms be included in the distributed software. They grant certain permissions to users, such as the right to use, copy, modify, and distribute the software.",
   };
 
-  // Check if choice exists in the mapping
-  if (additionalInfo.hasOwnProperty(choice)) {
-    // Set the result with choice and additional information
-    console.log("You selected '" + choice + "'. " + additionalInfo[choice]);
+  // Check if license exists in the mapping
+  if (addChoiceEnd.hasOwnProperty(license)) {
+    // Set the result with license and additional information
+    console.log("You selected '" + license + "'. " + addChoiceEnd[license]);
 
-    // Use choice in another JavaScript code
-    anotherFunction(choice);
+    // Use license in another JavaScript code
+    anotherFunction(license);
   } else {
-    // Invalid choice
-    console.log("Invalid choice. Please try again.");
+    // Invalid license
+    console.log("Invalid license. Please try again.");
   }
 }
 
-// Prompt the user for a choice and call the function
+// Prompt the user for a license and call the function
 inquirer
   .prompt([
     {
       type: 'input',
-      name: 'choice',
-      message: "Please enter your choice (A, B, C):",
+      name: 'license',
+      message: "Please enter your license (A, B, C):",
     }
   ])
   .then(answers => {
-    const userChoice = answers.choice;
-    handleChoice(userChoice);
+    const userChoice = answers.license;
+    chooseLicense(userChoice);
   })
   .catch(error => {
     console.log("Error occurred:", error);
   });
 
-  
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 
 // If there is no license, return an empty string
@@ -63,23 +62,23 @@ function generateMarkdown(data) {
 module.exports = generateMarkdown;
 
 
-// Prompt the user for a choice
-var choice = prompt("Please enter your choice (none, Apache License 2.0, MIT License, The Unlicensed):");
+// Prompt the user for a license
+var license = prompt("Please enter your license (none, Apache License 2.0, MIT License, The Unlicensed):");
 
-// Perform different actions based on the choice
-if (choice === "none") {
-  // Additional information for choice "none"
+// Perform different actions based on the license
+if (license === "none") {
+  // Additional information for license "none"
   console.log("You selected 'none'. Additional information for 'none'.");
-} else if (choice === "Apache License 2.0") {
-  // Additional information for choice "Apache License 2.0"
+} else if (license === "Apache License 2.0") {
+  // Additional information for license "Apache License 2.0"
   console.log("You selected 'Apache License 2.0'. Additional information for 'Apache License 2.0'.");
-} else if (choice === "MIT License") {
-  // Additional information for choice "MIT License"
+} else if (license === "MIT License") {
+  // Additional information for license "MIT License"
   console.log("You selected 'MIT License'. Additional information for 'MIT License'.");
-} else if (choice === "The Unlicensed") {
-  // Additional information for choice "The Unlicensed"
+} else if (license === "The Unlicensed") {
+  // Additional information for license "The Unlicensed"
   console.log("You selected 'The Unlicensed'. Additional information for 'The Unlicensed'.");
 } else {
-  // Invalid choice
-  console.log("Invalid choice. Please try again.");
+  // Invalid license
+  console.log("Invalid license. Please try again.");
 }

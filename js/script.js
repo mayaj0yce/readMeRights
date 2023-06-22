072cfe943efa5bc68227412d49f70fb1a357d63c
mayaj0yce <m.j.thayne@gmail.com>
2023-06-21 23:48:37 -0600

    pretty now

--- js/script.js
+++ js/script.js
@@ -93,92 +93,128 @@ switch (license) {
   }
 
 const htmlContent =  `<!DOCTYPE html>
-    <html lang="en">
-    
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
-        <title>${title}</title>
-    </head>
-    
-    <body>
-        <header class="p-5 mb-4 header bg-light">
-            <div class="container">
-            <h1 class="display-4">Project: ${title}</h1>
-        
-            <div class="licensingDisplay">
-            <a href="${licenseBadgeLink}">
-            <img src="${licenseBadgeLink}" alt="License Badge">
-            </a>
-            </div>
-        </header>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+    <title>${title}</title>
+    <style>
+        html,
+        body {
+            height: 100%;
+            background-color: beige;
+        }
+
+        body {
+
+            align-items: center;
+            justify-content: center;
+        }
+
+        .content-section {
+            margin-bottom: 30px;
+        }
+
+        body {
+            margin-bottom: 30px;
+            color: olivedrab;
+        }
+        .header {
+            text-align: right;
+            list-style-position: inside;
+        }
+    </style>
+</head>
+
+<body>
+    <header class="header">
         <div class="container">
-    
-            <h2>Table of Contents</h2>
-            <div>
-                <ul>
-                    <li><a href="description">Description</a></li>
-                    <li><a href="#installation">Installation</a></li>
-                    <li><a href="#usage">Usage</a></li>
-                    <li><a href="#test">Test</a></li>
-                    <li><a href="#contributing">Contributing</a></li>
-                    <li><a href="#license">License</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                </ul>
+            <h1 class="display-4 ">Project: ${title}</h1>
+            <div class="licensingDisplay">
+                <a href="${licenseBadgeLink}">
+                    <img src="${licenseBadgeLink}" alt="License Badge">
+                </a>
             </div>
+    </header>
+    <hr>
+    <div class="container">
+<div class="TOC">
+        <h2>Table of Contents</h2>
+        <div >
+            <ul>
+                <li><a href="description">Description</a></li>
+                <li><a href="#installation">Installation</a></li>
+                <li><a href="#usage">Usage</a></li>
+                <li><a href="#test">Test</a></li>
+                <li><a href="#contributing">Contributing</a></li>
+                <li><a href="#license">License</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+        <hr>
+        <div>
             <div>
                 <div class="container" id="description">
-                    <h2> ${description} </h2>
+                    <h2> Description: </h2>
+                    <h3> ${description} </h3>
                 </div>
-    
-    
+
+
                 <div class="container" id="installation">
-                    <h2> ${installation}</h2>
+                    <h2>To install:</h2>
+                    <h3> ${installation}</h3>
                 </div>
-    
+
                 <div class="container" id="usage">
-                    <h2> ${usage}</h2>
+                    <h2>This can be used for the following:</h2>
+                    <h3> ${usage}</h3>
                 </div>
-    
+
                 <div class="container" id="test">
-                    <h2> ${test}</h2>
+                    <h2>The following tests were preformed:</h2>
+                    <h3> ${test}</h3>
                 </div>
-    
+
                 <div class="container" id="contributing">
-                    <h2> ${contributing}</h2>
+                    <h2>If you would like to help contribute to this project you can: </h2>
+                    <h3> ${contributing}</h3>
                 </div>
-    
+
                 <div class="container" id="license">
                     <a href=" https://opensource.org/licenses/${license}"></a>
-                    <h2> ${licenseDescription}</h2>
-    
-                </div>
-    
-                <div class="container" id="contact">
-                    <h3>Questions? Contact Me</h3>
-                    <ul class="list-unstyled">
-                        <li>
-                            <a href="https://github.com/${gitHubLink}">GitHub Profile</a>
-                        </li>
-                        <li>
-                            <a
-                                href="mailto:${email}?subject=Question&body=I%20have%20a%20question%20regarding%20your%20project.">Send
-                                Email</a>
-                        </li>
-    
-                    </ul>
+                    <h2>My license option described:</h2>
+                    <h3> ${licenseDescription}</h3>
                 </div>
             </div>
+            <hr>
+            <div class="container text-center" id="contact">
+                <h3>Questions? Contact Me</h3>
+                <ul class="list-unstyled">
+                    <li>
+                        <a href="https://github.com/${gitHubLink}">This is my GitHub Profile</a>
+                    </li>
+                    <li>
+                        <a
+                            href="mailto:${email}?subject=Question&body=I%20have%20a%20question%20regarding%20your%20project.">Click
+                            Me To Send An
+                            Email</a>
+                    </li>
+
+                </ul>
+            </div>
         </div>
-    </body>
-    <footer class="footer">
-        <div class="container text-center">
-            <span>copyright @mayaj0yce 2023</span>
-        </div>
-    </footer>
-    
-    </html>`;
+    </div>
+</body>
+<footer class="footer">
+    <div class="container text-center">
+        <span>copyright @mayaj0yce 2023</span>
+    </div>
+</footer>
+
+</html>`;
  
     return htmlContent;
 };
